1a8397770fc111825d4aa28325757589147b017a
SoonKhen OwYong <dude@owyong.sk>
2017-10-23 22:49:20 +0800

    removes unused utility to grep the URL of kodi streaming

--- exodus_url.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-tail -n 100 ~/Library/Logs/kodi.log | grep "NOTICE: DVDPlayer: Opening:" | awk '{print $6}' | pbcopy
--- kodl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-tail -n 100 ~/Library/Logs/kodi.log | grep "NOTICE: DVDPlayer: Opening:" | awk '{print $6}' | xargs curl -C - -L > never_gonna_give_you_up.mp4
